32b13b668c53e8f6783e83d4a34f98ee82b63e44
ibai-omnidrone <ibai.jurado@omnidrone.net>
2020-06-08 16:47:13 +0200

    add userSetup.py logic

--- scripts/userSetup.py
+++ scripts/userSetup.py
@@ -1,5 +1,17 @@
-import pyblish.api
-import pyblish_maya
+import maya.utils
+print(".... ENTERING USER SETUP.....")
 
-pyblish.api.register_gui("pyblish_lite")  # Fallback
-pyblish_maya.setup()
+
+def delayedStartup():
+    import pyblish.api
+    import pyblish_maya
+    pyblish.api.register_gui('pyblish_lite')
+    pyblish_maya.setup()
+
+
+if __name__ == "__main__":
+    print("-------EXECUTE MAIN-----")
+    try:
+        maya.utils.executeDeferred(delayedStartup)
+    except:
+        print("ERROR: Something went wrong loading pyblish module.")
